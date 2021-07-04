f8906ba4a037a306cb2835a680d628184ec007ba
harshapulikollu <harshapulikollu@outlook.com>
2021-07-04 10:41:39 +0530

    Added alt for images in more apps modal

--- src/components/MoreAppsModal/MoreAppsModal.js
+++ src/components/MoreAppsModal/MoreAppsModal.js
@@ -9,27 +9,27 @@ export default function MoreAppsModal(props) {
             <div className="modal-content">
                 <div className="group">
                     <div className="item">
-                        <img src="https://image.pngaaa.com/572/3306572-middle.png" width="50" height="50" />
+                        <img src="https://image.pngaaa.com/572/3306572-middle.png" alt="here" width="50" height="50" />
                         <span>YouTube</span>
                     </div>
                     <div className="item">
-                        <img src="https://image.pngaaa.com/572/3306572-middle.png" width="50" height="50" />
+                        <img src="https://image.pngaaa.com/572/3306572-middle.png" alt="here" width="50" height="50" />
                         <span>YouTube</span>
                     </div>
                     <div className="item">
-                        <img src="https://image.pngaaa.com/572/3306572-middle.png" width="50" height="50" />
+                        <img src="https://image.pngaaa.com/572/3306572-middle.png" alt="here" width="50" height="50" />
                         <span>YouTube</span>
                     </div>
                     <div className="item">
-                        <img src="https://image.pngaaa.com/572/3306572-middle.png" width="50" height="50" />
+                        <img src="https://image.pngaaa.com/572/3306572-middle.png" alt="here" width="50" height="50" />
                         <span>YouTube</span>
                     </div>
                     <div className="item">
-                        <img src="https://image.pngaaa.com/572/3306572-middle.png" width="50" height="50" />
+                        <img src="https://image.pngaaa.com/572/3306572-middle.png" alt="here" width="50" height="50" />
                         <span>YouTube</span>
                     </div>
                     <div className="item">
-                        <img src="https://image.pngaaa.com/572/3306572-middle.png" width="50" height="50" />
+                        <img src="https://image.pngaaa.com/572/3306572-middle.png" alt="here" width="50" height="50" />
                         <span>YouTube</span>
                     </div>
                 </div>
