a22c7493bed1e997d5ab5dbf874f70f2a41da280
Harsha Pulikollu <harshapulikollu@outlook.com>
2023-04-20 19:35:54 +0530

    Updated Description

--- src/components/BreifInfo/BreifInfo.js
+++ src/components/BreifInfo/BreifInfo.js
@@ -5,7 +5,7 @@ export default function BreifInfo() {
         <div className="header">
            <div className="name-work">
            <span className="name">Harsha Pulikollu</span>
-           <span className="work">Full Stack Developer</span>
+           <span className="work">Flutter Developer</span>
            </div>
             <img src="https://avatars.githubusercontent.com/u/22256553?v=4" width="100" height="100" />
         </div>
@@ -13,13 +13,13 @@ export default function BreifInfo() {
         <div className="text">
             <p>
                 Harsha Pulikollu is a 24 years old full stack developer currenlty working at DXC Technology. 
-                I'm tech entusiastic and loves coding and developing things. 
+                I'm tech entusiastic and loves coding and developing apps and webapps. 
             </p>
-            <p>Harsha familiar with <strong>React</strong>, <strong>Node.js</strong>, <strong>Flutter</strong>, <strong>Javascript</strong>, <strong>Java</strong> frameworks and programing languages.</p>
+            <p>Harsha familiar with <strong>Flutter</strong>, <strong>React</strong>, <strong>Node.js</strong>, <strong>Javascript</strong>, <strong>GoLang</strong> frameworks and programing languages.</p>
             <p><strong>Born:</strong> 23rd January 1997</p>
             <p><strong>Location:</strong> Nellore, Andhra Pradesh</p>
-            <p><strong>Current Company:</strong> DXC Technology</p>
-            <p><strong>Work Location:</strong> Chennai, TamilNadu</p>
+            <p><strong>Current Company:</strong> MoneyPlanned</p>
+            <p><strong>Work Location:</strong> Remote</p>
         </div>
         <div className="horiz-st-line"></div>
         <div className="social-links">
